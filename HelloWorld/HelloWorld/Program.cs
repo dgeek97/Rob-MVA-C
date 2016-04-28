ad3310d796e64d33f7641e13ef02a6b054fdfe5e
Rob Walker <robert.walker@xaviussoft.com>
2016-04-27 19:58:02 -0500

    New App

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            float xyz = 0;
+
+            while (xyz < 2)
+            {
+                xyz = float.Parse(Console.ReadLine());
+                Console.WriteLine(xyz);
+                // Test
+                
+
+
+
+            } 
+
+
+
+
+        }
+    }
+}
